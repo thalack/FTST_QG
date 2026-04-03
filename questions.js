e570a0d5f8484965461c6b44c28158de25d8ae20
thalack <thalack@gmail.com>
2026-04-03 20:13:18 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,260 +1,85 @@
-const questionbank = 
-// PASTE YOUR CONVERTED DATA FROM CSVJSON.COM HERE
-;
-[
-  {
-    "Medium talk": "What makes you weird?"
-  },
-  {
-    "Medium talk": "What takes up your headspace these days?"
-  },
-  {
-    "Medium talk": "What do you value most in a friendship? How hard is this to find?"
-  },
-  {
-    "Medium talk": "Tell us in detail about your happy place. Close your eyes and imagine you’re there."
-  },
-  {
-    "Medium talk": "If a museum had an exhibit about you, what items would be put on display?"
-  },
-  {
-    "Medium talk": "What’s something you’ve talked too much about doing and not done?"
-  },
-  {
-    "Medium talk": "If you came with a warning label, what would yours say?"
-  },
-  {
-    "Medium talk": "What’s your superpower? How do you use it?"
-  },
-  {
-    "Medium talk": "What makes you forget to eat and pee?"
-  },
-  {
-    "Medium talk": "How do you recharge when you’re stressed out?"
-  },
-  {
-    "Medium talk": "What’s the last item you crossed off your bucket list? What’s the next item on your bucket list?"
-  },
-  {
-    "Medium talk": "What’s one of your favorite smells? What feelings does it come with?"
-  },
-  {
-    "Medium talk": "What was the best part of growing up in your hometown?"
-  },
-  {
-    "Medium talk": "What’s a song that instantly shifts the molecules in your body? Tell the story behind it."
-  },
-  {
-    "Medium talk": "If you were an animal, which one would you be? Wait, but why?"
-  },
-  {
-    "Medium talk": "How would your closest friend describe you?"
-  },
-  {
-    "Medium talk": "What are you pumped about these days?"
-  },
-  {
-    "Medium talk": "What do the first hours of your day usually look like?"
-  },
-  {
-    "Medium talk": "Your Instagram has more followers than anyone else in the world. What is your channel famous for?"
-  },
-  {
-    "Medium talk": "When was the last time you did something for the first time? How did it feel?"
-  },
-  {
-    "Medium talk": "What’s a unique personality trait that you haven’t inherited from your parents?"
-  },
-  {
-    "Medium talk": "Soul talk"
-  },
-  {
-    "Medium talk": "What’s the nicest and meanest voice in your head? What do they say? Who do you believe?"
-  },
-  {
-    "Medium talk": "What’s a lesson you learned the hard way?"
-  },
-  {
-    "Medium talk": "What or who makes you feel big? What or who makes you feel small?"
-  },
-  {
-    "Medium talk": "How has your perspective on life changed over the years?"
-  },
-  {
-    "Medium talk": "What does success mean to you?"
-  },
-  {
-    "Medium talk": "What’s a challenge you’ve overcome that has shaped who you are?"
-  },
-  {
-    "Medium talk": "What’s an experience that has significantly changed your life?"
-  },
-  {
-    "Medium talk": "When do you feel most alive?"
-  },
-  {
-    "Medium talk": "How do you find meaning in difficult times?"
-  },
-  {
-    "Medium talk": "What’s a fear you’ve conquered? What’s one you have yet to conquer?"
-  },
-  {
-    "Medium talk": "What is a value that is true to you? How has it influenced your life?"
-  },
-  {
-    "Medium talk": "How has the definition of happiness evolved for you?"
-  },
-  {
-    "Medium talk": "What is the biggest risk you’ve taken? If you had another chance, would you do anything differently?"
-  },
-  {
-    "Medium talk": "What has to happen for you to get defensive?"
-  },
-  {
-    "Medium talk": "Tell us about a moment that helped you believe in love."
-  },
-  {
-    "Medium talk": "Who has loved you the most? How did they love you?"
-  },
-  {
-    "Medium talk": "How do you self‐sabotage?"
-  },
-  {
-    "Medium talk": "Who do you admire? What qualities do you admire in them?"
-  },
-  {
-    "Medium talk": "What are you famous for among your friends and family?"
-  },
-  {
-    "Medium talk": "If we interviewed your exes, what would they say about you?"
-  },
-  {
-    "Medium talk": "How do you like to help people?"
-  },
-  {
-    "Medium talk": "What was the role you played in your family as a kid? What about now?"
-  },
-  {
-    "Medium talk": "What’s an experience you think everyone should have before they die?"
-  },
-  {
-    "Medium talk": "What book or idea deeply shifted how you think about something?"
-  },
-  {
-    "Medium talk": "What is one of the first things you notice about people?"
-  },
-  {
-    "Medium talk": "What’s something you know you do differently than most people?"
-  },
-  {
-    "Medium talk": "How paranoid are you? What are you most paranoid about?"
-  },
-  {
-    "Medium talk": "What is something you would usually avoid sharing about yourself when you first meet someone?"
-  },
-  {
-    "Medium talk": "What has life been trying to teach you recently?"
-  },
-  {
-    "Medium talk": "What do you think people first notice about you?"
-  },
-  {
-    "Medium talk": "Looking back, what childhood adventures are impossible to forget?"
-  },
-  {
-    "Medium talk": "What message would you put on a billboard for thousands of people to see every day?"
-  },
-  {
-    "Medium talk": "What did you learn about yourself from your last relationship?"
-  },
-  {
-    "Medium talk": "What’s something you’re proud of that you don’t usually get to share with people?"
-  },
-  {
-    "Medium talk": "What is something you’d whisper in the ear of your younger self?"
-  },
-  {
-    "Medium talk": "Philosophical questions"
-  },
-  {
-    "Medium talk": "If you could invent something that would make life easier for people, what would you invent?"
-  },
-  {
-    "Medium talk": "What’s something that most people don’t believe that you believe?"
-  },
-  {
-    "Medium talk": "If life is meant for living, what does it mean to you to truly live?"
-  },
-  {
-    "Medium talk": "If you could change one aspect of human nature, what would it be?"
-  },
-  {
-    "Medium talk": "Imagine you’re all stranded on an exotic island. How would you use your skills and talents to survive?"
-  },
-  {
-    "Medium talk": "What would be the most surprising scientific discovery imaginable?"
-  },
-  {
-    "Medium talk": "What social traditions do you think are outdated, and what new ones should we adopt? Why?"
-  },
-  {
-    "Medium talk": "Do you think AI can ever understand human emotions?"
-  },
-  {
-    "Medium talk": "What role do you think dreams play in our lives? Are they random or something more?"
-  },
-  {
-    "Medium talk": "Is there a purpose to life? Or is this all just one big show?"
-  },
-  {
-    "Medium talk": "What is the fastest way of getting to know a complete stranger?"
-  },
-  {
-    "Medium talk": "What animal do you think would be the most interesting to communicate with and why?"
-  },
-  {
-    "Medium talk": "What is a book you think should be mandatory in school? Why?"
-  },
-  {
-    "Medium talk": "What role does sex play in the success of a relationship?"
-  },
-  {
-    "Medium talk": "How does the perception of time shape our life?"
-  },
-  {
-    "Medium talk": "Would knowing the future enhance or hinder personal growth and decision‐making?"
-  },
-  {
-    "Medium talk": "How would relationships change if we all had the power of telepathy?"
-  },
-  {
-    "Medium talk": "If you had the power to solve one of the world’s biggest problems, which one would you choose and why?"
-  },
-  {
-    "Medium talk": "What is the biggest change you notice from your parents’ generation to yours?"
-  },
-  {
-    "Medium talk": "How do you think the concept of work will evolve in the next 20 years?"
-  },
-  {
-    "Medium talk": "What is something most people chase that is totally overrated?"
-  },
-  {
-    "Medium talk": "What do you think is out there?"
-  },
-  {
-    "Medium talk": "If a crystal ball could tell you the truth about yourself, your life, the future, or anything else, what would you want to know?"
-  },
-  {
-    "Medium talk": "What do you think déjà vu is?"
-  },
-  {
-    "Medium talk": "What is the most significant invention in human history?"
-  },
-  {
-    "Medium talk": "If you could instantly become an expert in something, what would it be?"
-  },
-  {
-    "Medium talk": "If you could create a random holiday, what would you call it, what day would it be on, and what would you do?"
-  }
-]
+const questionbank = [
+  { "Category": "Medium talk", "Text": "What makes you weird?" },
+  { "Category": "Medium talk", "Text": "What takes up your headspace these days?" },
+  { "Category": "Medium talk", "Text": "What do you value most in a friendship? How hard is this to find?" },
+  { "Category": "Medium talk", "Text": "Tell us in detail about your happy place. Close your eyes and imagine you’re there." },
+  { "Category": "Medium talk", "Text": "If a museum had an exhibit about you, what items would be put on display?" },
+  { "Category": "Medium talk", "Text": "What’s something you’ve talked too much about doing and not done?" },
+  { "Category": "Medium talk", "Text": "If you came with a warning label, what would yours say?" },
+  { "Category": "Medium talk", "Text": "What’s your superpower? How do you use it?" },
+  { "Category": "Medium talk", "Text": "What makes you forget to eat and pee?" },
+  { "Category": "Medium talk", "Text": "How do you recharge when you’re stressed out?" },
+  { "Category": "Medium talk", "Text": "What’s the last item you crossed off your bucket list? What’s the next item on your bucket list?" },
+  { "Category": "Medium talk", "Text": "What’s one of your favorite smells? What feelings does it come with?" },
+  { "Category": "Medium talk", "Text": "What was the best part of growing up in your hometown?" },
+  { "Category": "Medium talk", "Text": "What’s a song that instantly shifts the molecules in your body? Tell the story behind it." },
+  { "Category": "Medium talk", "Text": "If you were an animal, which one would you be? Wait, but why?" },
+  { "Category": "Medium talk", "Text": "How would your closest friend describe you?" },
+  { "Category": "Medium talk", "Text": "What are you pumped about these days?" },
+  { "Category": "Medium talk", "Text": "What do the first hours of your day usually look like?" },
+  { "Category": "Medium talk", "Text": "Your Instagram has more followers than anyone else in the world. What is your channel famous for?" },
+  { "Category": "Medium talk", "Text": "When was the last time you did something for the first time? How did it feel?" },
+  { "Category": "Medium talk", "Text": "What’s a unique personality trait that you haven’t inherited from your parents?" },
+  { "Category": "Soul talk", "Text": "What’s the nicest and meanest voice in your head? What do they say? Who do you believe?" },
+  { "Category": "Soul talk", "Text": "What’s a lesson you learned the hard way?" },
+  { "Category": "Soul talk", "Text": "What or who makes you feel big? What or who makes you feel small?" },
+  { "Category": "Soul talk", "Text": "How has your perspective on life changed over the years?" },
+  { "Category": "Soul talk", "Text": "What does success mean to you?" },
+  { "Category": "Soul talk", "Text": "What’s a challenge you’ve overcome that has shaped who you are?" },
+  { "Category": "Soul talk", "Text": "What’s an experience that has significantly changed your life?" },
+  { "Category": "Soul talk", "Text": "When do you feel most alive?" },
+  { "Category": "Soul talk", "Text": "How do you find meaning in difficult times?" },
+  { "Category": "Soul talk", "Text": "What’s a fear you’ve conquered? What’s one you have yet to conquer?" },
+  { "Category": "Soul talk", "Text": "What is a value that is true to you? How has it influenced your life?" },
+  { "Category": "Soul talk", "Text": "How has the definition of happiness evolved for you?" },
+  { "Category": "Soul talk", "Text": "What is the biggest risk you’ve taken? If you had another chance, would you do anything differently?" },
+  { "Category": "Soul talk", "Text": "What has to happen for you to get defensive?" },
+  { "Category": "Soul talk", "Text": "Tell us about a moment that helped you believe in love." },
+  { "Category": "Soul talk", "Text": "Who has loved you the most? How did they love you?" },
+  { "Category": "Soul talk", "Text": "How do you self‐sabotage?" },
+  { "Category": "Soul talk", "Text": "Who do you admire? What qualities do you admire in them?" },
+  { "Category": "Soul talk", "Text": "What are you famous for among your friends and family?" },
+  { "Category": "Soul talk", "Text": "If we interviewed your exes, what would they say about you?" },
+  { "Category": "Soul talk", "Text": "How do you like to help people?" },
+  { "Category": "Soul talk", "Text": "What was the role you played in your family as a kid? What about now?" },
+  { "Category": "Soul talk", "Text": "What’s an experience you think everyone should have before they die?" },
+  { "Category": "Soul talk", "Text": "What book or idea deeply shifted how you think about something?" },
+  { "Category": "Soul talk", "Text": "What is one of the first things you notice about people?" },
+  { "Category": "Soul talk", "Text": "What’s something you know you do differently than most people?" },
+  { "Category": "Soul talk", "Text": "How paranoid are you? What are you most paranoid about?" },
+  { "Category": "Soul talk", "Text": "What is something you would usually avoid sharing about yourself when you first meet someone?" },
+  { "Category": "Soul talk", "Text": "What has life been trying to teach you recently?" },
+  { "Category": "Soul talk", "Text": "What do you think people first notice about you?" },
+  { "Category": "Soul talk", "Text": "Looking back, what childhood adventures are impossible to forget?" },
+  { "Category": "Soul talk", "Text": "What message would you put on a billboard for thousands of people to see every day?" },
+  { "Category": "Soul talk", "Text": "What did you learn about yourself from your last relationship?" },
+  { "Category": "Soul talk", "Text": "What’s something you’re proud of that you don’t usually get to share with people?" },
+  { "Category": "Soul talk", "Text": "What is something you’d whisper in the ear of your younger self?" },
+  { "Category": "Philosophical questions", "Text": "If you could invent something that would make life easier for people, what would you invent?" },
+  { "Category": "Philosophical questions", "Text": "What’s something that most people don’t believe that you believe?" },
+  { "Category": "Philosophical questions", "Text": "If life is meant for living, what does it mean to you to truly live?" },
+  { "Category": "Philosophical questions", "Text": "If you could change one aspect of human nature, what would it be?" },
+  { "Category": "Philosophical questions", "Text": "Imagine you’re all stranded on an exotic island. How would you use your skills and talents to survive?" },
+  { "Category": "Philosophical questions", "Text": "What would be the most surprising scientific discovery imaginable?" },
+  { "Category": "Philosophical questions", "Text": "What social traditions do you think are outdated, and what new ones should we adopt? Why?" },
+  { "Category": "Philosophical questions", "Text": "Do you think AI can ever understand human emotions?" },
+  { "Category": "Philosophical questions", "Text": "What role do you think dreams play in our lives? Are they random or something more?" },
+  { "Category": "Philosophical questions", "Text": "Is there a purpose to life? Or is this all just one big show?" },
+  { "Category": "Philosophical questions", "Text": "What is the fastest way of getting to know a complete stranger?" },
+  { "Category": "Philosophical questions", "Text": "What animal do you think would be the most interesting to communicate with and why?" },
+  { "Category": "Philosophical questions", "Text": "What is a book you think should be mandatory in school? Why?" },
+  { "Category": "Philosophical questions", "Text": "What role does sex play in the success of a relationship?" },
+  { "Category": "Philosophical questions", "Text": "How does the perception of time shape our life?" },
+  { "Category": "Philosophical questions", "Text": "Would knowing the future enhance or hinder personal growth and decision‐making?" },
+  { "Category": "Philosophical questions", "Text": "How would relationships change if we all had the power of telepathy?" },
+  { "Category": "Philosophical questions", "Text": "If you had the power to solve one of the world’s biggest problems, which one would you choose and why?" },
+  { "Category": "Philosophical questions", "Text": "What is the biggest change you notice from your parents’ generation to yours?" },
+  { "Category": "Philosophical questions", "Text": "How do you think the concept of work will evolve in the next 20 years?" },
+  { "Category": "Philosophical questions", "Text": "What is something most people chase that is totally overrated?" },
+  { "Category": "Philosophical questions", "Text": "What do you think is out there?" },
+  { "Category": "Philosophical questions", "Text": "If a crystal ball could tell you the truth about yourself, your life, the future, or anything else, what would you want to know?" },
+  { "Category": "Philosophical questions", "Text": "What do you think déjà vu is?" },
+  { "Category": "Philosophical questions", "Text": "What is the most significant invention in human history?" },
+  { "Category": "Philosophical questions", "Text": "If you could instantly become an expert in something, what would it be?" },
+  { "Category": "Philosophical questions", "Text": "If you could create a random holiday, what would you call it, what day would it be on, and what would you do?" }
+];
